1bd35c642a7ed97d57c71561b1fbfd3343b59de6
Sergey <enotagressor@gmail.com>
2022-05-13 23:10:46 +0700

    DZIspravlena

--- src/Print.java
+++ src/Print.java
@@ -6,9 +6,9 @@ public class Print {
         Book book1 = new Book("Виват", 1015, author1);
         Book book2 = new Book("Салам", 2015, author2);
 
-        System.out.println("newBook = " + book1.getName() +" г.и. " + book1.getPublishingYear() + " Автор: " + book1.getAuthor());
-        System.out.println("newBook = " + book2.getName() +" г.и. " + book2.getPublishingYear() + " Автор: " + book2.getAuthor());
+        System.out.println(book1.getAuthor().getAuthorName() + " " +  book1.getAuthor().getAuthorFamily() +": " + book1.getName() +": " + book1.getPublishingYear());
+        System.out.println(book2.getAuthor().getAuthorName() + " " +  book2.getAuthor().getAuthorFamily() +": " + book2.getName() +": " + book2.getPublishingYear());
         book1.setPublishingYear(2002);
-        System.out.println("newBook = " + book1.getName() +" г.и. " + book1.getPublishingYear() + " Автор: " + book1.getAuthor());
+        System.out.println(book1.getAuthor().getAuthorName() + " " +  book1.getAuthor().getAuthorFamily() +": " + book1.getName() +": " + book1.getPublishingYear());
     }
 }
